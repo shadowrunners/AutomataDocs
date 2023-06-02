766e7c250840de2bf7abe10eded8025a38e000a3
scrappie <scr3ppie@gmail.com>
2023-06-02 17:37:00 +0300

    chore: add docs files

---
+++ app.config.ts
@@ -0,0 +1,39 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Automata',
+    description: 'Harmonize the rythm of sound.',
+    socials: {
+      twitter: 'scr3ppie',
+      github: 'shadowrunners/automata'
+    },
+    github: {
+      branch: 'docs',
+      repo: 'automata',
+      owner: 'shadowrunners',
+      edit: true
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: true
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: [],
+      fluid: true
+    },
+    footer: {
+      iconLinks: [
+        {
+          href: 'https://nuxt.com',
+          icon: 'simple-icons:nuxtdotjs'
+        }
+      ]
+    }
+  }
+})
